aa8215594f72ce14dfda8ef1dfc007f64378657c
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2020-09-24 08:33:40 +0330

    Prepare outlines

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './App.sass';
 
 function App() {
-  return <div className="container">Customer Base</div>;
+  return (
+    <div className="container">
+      <section className="wideContainer">
+        <h6>Customer Base</h6>
+      </section>
+      <section className="wideContainer">
+        <h6>New vs. returning customers</h6>
+      </section>
+      <section className="narrowContainer">
+        <h6>New customers</h6>
+      </section>
+      <section className="narrowContainer">
+        <h6>Returning customers</h6>
+      </section>
+      <section className="narrowContainer">
+        <h6>Re-order percentage</h6>
+      </section>
+      <section className="narrowContainer">
+        <h6>Wolt rating</h6>
+      </section>
+    </div>
+  );
 }
 
 export default App;

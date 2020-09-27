e12c5bcca91320620dd652fc6c098231eacf2235
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2020-09-27 14:37:15 +0330

    Fixes responsive behavior of pie charts

--- src/components/PercentageViewer.js
+++ src/components/PercentageViewer.js
@@ -27,33 +27,31 @@ const PercentageViewer = ({
         <p>{baseTitle}</p>
         <p style={{ color: 'black' }}>{baseValue}</p>
       </div>
-      <div className={styles.chartContainer}>
-        <ResponsiveContainer height={200} width="100%">
-          <PieChart>
-            <Pie
-              data={data}
-              dataKey="value"
-              innerRadius={70}
-              outerRadius={80}
-              paddingAngle={1}
-              startAngle={-270}
-              endAngle={-630}
-            >
-              {data.map((_, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
-        <div className={styles.label}>
-          <p>{percentageTitle}</p>
-          <h1>
-            {(showSign && percentageValue > 0 && '+') || (percentageValue < 0 && '-')}
-            {showPercent
-              ? `${Math.round(Math.abs(percentageValue)).toFixed(2)}%`
-              : `${Math.abs(percentageValue).toFixed(2)}`}
-          </h1>
-        </div>
+      <ResponsiveContainer height={200} width="100%">
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            innerRadius={70}
+            outerRadius={80}
+            paddingAngle={1}
+            startAngle={-270}
+            endAngle={-630}
+          >
+            {data.map((_, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <div className={styles.label}>
+        <p>{percentageTitle}</p>
+        <h1>
+          {(showSign && percentageValue > 0 && '+') || (percentageValue < 0 && '-')}
+          {showPercent
+            ? `${Math.round(Math.abs(percentageValue)).toFixed(2)}%`
+            : `${Math.abs(percentageValue).toFixed(2)}`}
+        </h1>
       </div>
       <p className={styles.description}>{description}</p>
     </section>

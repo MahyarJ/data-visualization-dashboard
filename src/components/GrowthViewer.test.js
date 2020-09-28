1e3e9b3b68cd899512c19daa67fb5306cd2a6bf4
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2020-09-28 23:15:54 +0330

    Add more tests

--- src/components/GrowthViewer.test.js
+++ src/components/GrowthViewer.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import GrowthViewer from './GrowthViewer';
 
-test('renders title correctly', () => {
-  const { getByText } = render(<GrowthViewer title="Foo" />);
-  const titleElement = getByText('Foo');
-  expect(titleElement).toBeInTheDocument();
-});
+describe('GrowthViewer', () => {
+  it('renders title correctly', () => {
+    const { getByText } = render(<GrowthViewer title="Foo" />);
+    const titleElement = getByText('Foo');
+    expect(titleElement).toBeInTheDocument();
+  });
 
-test('renders growth truely', () => {});
-test('renders basket-size difference truely', () => {});
+  it('renders growth truely', () => {
+    const recentValue = 100;
+    const prevValue = 10;
+    const { getByText } = render(
+      <GrowthViewer title="Foo" recentValue={recentValue} prevValue={prevValue} />,
+    );
+    const growth = ((recentValue - prevValue) * 100) / prevValue;
+    var regex = new RegExp(Math.round(Math.abs(growth)).toFixed(2) + '%', 'i');
+    expect(getByText(regex)).toBeInTheDocument();
+  });
+});
--- src/components/PercentageViewer.test.js
+++ src/components/PercentageViewer.test.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import PercentageViewer from './PercentageViewer';
 
-test('renders title correctly', () => {
-  const { getByText } = render(<PercentageViewer title="Wolt rating" />);
-  const titleElement = getByText('Wolt rating');
-  expect(titleElement).toBeInTheDocument();
-});
+describe('PercentageViewer', () => {
+  it('renders title correctly', () => {
+    const { getByText } = render(<PercentageViewer title="Wolt rating" />);
+    const titleElement = getByText('Wolt rating');
+    expect(titleElement).toBeInTheDocument();
+  });
 
-test('do not renders % when showPercent is false', () => {
-  const { queryByText } = render(<PercentageViewer title="Wolt rating" />);
-  expect(queryByText(/%/i)).toBeNull();
-});
+  it('do not renders % when showPercent is false', () => {
+    const { queryByText } = render(<PercentageViewer title="Wolt rating" />);
+    expect(queryByText(/%/i)).toBeNull();
+  });
 
-test('renders % when showPercent is true', () => {
-  const { queryByText } = render(
-    <PercentageViewer title="Wolt rating" showPercent={true} />,
-  );
-  expect(queryByText(/%/i)).toBeTruthy();
+  it('renders % when showPercent is true', () => {
+    const { queryByText } = render(
+      <PercentageViewer title="Wolt rating" showPercent={true} />,
+    );
+    expect(queryByText(/%/i)).toBeTruthy();
+  });
 });
